e960535e1e917f6c95ca7718190ef4ecd2f3cb15
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2016-05-21 00:58:07 +0200

    test parser on example file

--- src/test/scala/eu/nomad_lab/parsers/CrystalParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/CrystalParserSpec.scala
@@ -5,11 +5,11 @@ import org.specs2.mutable.Specification
 object CrystalParserSpec extends Specification {
   "CrystalParserTest" >> {
     "test with json-events" >> {
-      ParserRun.parse(CrystalParser, "parsers/crystal/test/examples/...", "json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(CrystalParser, "parsers/crystal/test/examples/mgo/mgo.out", "json-events") must_== ParseResult.ParseSuccess
     }
   }
 
   "test with json" >> {
-    ParserRun.parse(CrystalParser, "parsers/crystal/test/examples/...", "json") must_== ParseResult.ParseSuccess
+    ParserRun.parse(CrystalParser, "parsers/crystal/test/examples/mgo/mgo.out", "json") must_== ParseResult.ParseSuccess
   }
 }
