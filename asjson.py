6f5590b5a149f4a077054092ea1d1c8ad08e0136
Sami Kivistö <sami.k.kivisto@aalto.fi>
2016-07-15 00:49:29 +0300

    Crystal parser now works with python3

--- asjson.py
+++ asjson.py
@@ -8,7 +8,7 @@ for path in paths:
     # 1. Initialize a parser by giving a path to the CRYSTAL output file and a list of
     # default units
     #path = "test/examples/NaCl/NaCl.out"
-    print "Parsing: " + path
+    print("Parsing: " + path)
     parser = CrystalParser(path, default_units=["eV"], backend=JsonParseEventsWriterBackend)
     # 2. Parse
     parser.parse()
