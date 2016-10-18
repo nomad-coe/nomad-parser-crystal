e20dd62e02072d9a5c735d7f137dab2d19c174c2
Lauri Himanen <lauri.himanen@aalto.fi>
2016-10-18 08:59:43 +0300

    Added a DFT example, configured it as a test case.

--- src/test/scala/eu/nomad_lab/parsers/CrystalParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/CrystalParserSpec.scala
@@ -5,11 +5,11 @@ import org.specs2.mutable.Specification
 object CrystalParserSpec extends Specification {
   "CrystalParserTest" >> {
     "test with json-events" >> {
-      ParserRun.parse(CrystalParser, "parsers/crystal/test/examples/mgo/mgo.out", "json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(CrystalParser, "parsers/crystal/test/examples/single_point/si.out", "json-events") must_== ParseResult.ParseSuccess
     }
   }
 
   "test with json" >> {
-    ParserRun.parse(CrystalParser, "parsers/crystal/test/examples/mgo/mgo.out", "json") must_== ParseResult.ParseSuccess
+    ParserRun.parse(CrystalParser, "parsers/crystal/test/examples/single_point/si.out", "json") must_== ParseResult.ParseSuccess
   }
 }
